7ed6178876c499261fb4c5b9ef2b7411f8719101
HathawayJA <Hathawayja@gmail.com>
2022-04-01 23:21:25 -0700

    Pathing for Death
    basic path finding for death.

--- components/Death/Death.gd
+++ components/Death/Death.gd
@@ -1,38 +1,42 @@
 extends KinematicBody2D
-class_name Player
+class_name Death
 
 var velocity := Vector2.ZERO
-var speed := 200.0
+var speed := 150.0
 var acceleration := 0.2
 var state = "idle-forward"
 var flipped = false
+var path := PoolVector2Array()
+var player = null
+
 
 func _ready():
+	player = get_tree().get_nodes_in_group("player")[0]
 	pass
 
 func _process(_delta):
-	velocity = Vector2.ZERO
-	var new_state = 'idle-forward'
 	
-	if Input.is_action_pressed("up"):
-		velocity.y -= 1.0
-		new_state = 'move-up'
-	elif Input.is_action_pressed("down"):
-		velocity.y += 1.0
-		new_state = 'move-down'
-	if Input.is_action_pressed("left"):
-		velocity.x -= 1.0
-		new_state = 'move-side'
-		flipped = true
-	elif Input.is_action_pressed("right"):
-		velocity.x += 1.0
-		new_state = 'move-side'
-		flipped = false
 	
-	_change_state(new_state)
-	$AnimatedSprite.flip_h = flipped
+	path = self.get_parent().get_simple_path(self.position, player.position)
 	
-	velocity = velocity.normalized() * speed
+	# Calculate the movement distance for this frame
+	var distance_to_walk = speed * _delta
+	
+	# Move the player along the path until he has run out of movement or the path ends.
+	while distance_to_walk > 0 and path.size() > 0:
+		var distance_to_next_point = position.distance_to(path[0])
+		if distance_to_walk <= distance_to_next_point:
+			# The player does not have enough movement left to get to the next point.
+			position += position.direction_to(path[0]) * distance_to_walk
+		else:
+			# The player get to the next point
+			position = path[0]
+			path.remove(0)
+		# Update the distance to walk
+		distance_to_walk -= distance_to_next_point
+
+	
+	#velocity = velocity.normalized() * speed
 
 # TODO(koger): Movement is snappy. Is this desirable? Do we want acceleration,
 # sliding, and other effects that make it feel more slugish?
@@ -43,3 +47,6 @@ func _change_state(new_state):
 	if new_state != state:
 		state = new_state
 		#$AnimatedSprite.play(state)
+		
+		
+
