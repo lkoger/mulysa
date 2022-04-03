b80545e6af60ffc626ccd3f0b9f16cafe79aeb36
Levi Koger <levikoger@gmail.com>
2022-04-03 11:22:18 -0500

    Add audio fixes and clean up menu button code

--- ui/components/MenuButton.gd
+++ ui/components/MenuButton.gd
@@ -2,19 +2,14 @@ extends Button
 
 export(String, FILE, "*.tscn") var next_scene
 
-func _ready() -> void:
-	connect("pressed", self, "_on_menu_button_pressed", [next_scene])
-	pass
-
-func _on_mouse_entered() -> void:
-	Globals._play('hover')
-
-
-func _on_button_up() -> void:
+func _on_MenuButton_button_up():
 	Globals._play('select')
 	get_tree().change_scene(next_scene)
 
-
-func _on_NewGameButton_button_up() -> void:
+func _on_NewGameButton_button_up():
 	Globals._play('light-off')
 	Globals._play('level-light-hum')
+
+
+func _on_MenuButton_mouse_entered():
+	Globals._play('hover')
--- ui/title_screen/TitleScreen.gd
+++ ui/title_screen/TitleScreen.gd
@@ -4,8 +4,10 @@ func _ready() -> void:
 	Globals._play('light-on')
 	Globals._play('title-light-hum')
 	# Connect the pressed() method of every scene to this scene
-	for button in get_tree().get_nodes_in_group("button"):
-		button.connect("pressed", self, "_on_menu_button_pressed", [button.next_scene])
+#	for button in get_tree().get_nodes_in_group("button"):
+#		button.connect("pressed", self, "_on_menu_button_pressed", [button.next_scene])
 
-func _on_menu_button_pressed(next_scene):
-	get_tree().change_scene(next_scene)
+
+func _on_NewGameButton_button_up() -> void:
+	Globals._play('light-off')
+	Globals._play('level-light-hum')
